f9ab7f2e5bb7f4bfb47557f2986878b5d3228105
DavisVaughan <davis@rstudio.com>
2019-03-02 22:10:27 -0500

    Add addr_add_one()

--- src/add.c
+++ src/add.c
@@ -1,3 +1,14 @@
 #define R_NO_REMAP
 #include <R.h>
 #include <Rinternals.h>
+
+SEXP addr_add_one(SEXP a) {
+
+  SEXP out = PROTECT(Rf_allocVector(REALSXP, 1));
+
+  REAL(out)[0] = Rf_asReal(a) + 1;
+
+  UNPROTECT(1);
+
+  return out;
+}
